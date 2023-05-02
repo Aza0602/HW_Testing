41c09498bb3aca91f891cdb3d1b8eb85b3f748c2
Azat <aelchikushuulu@gmail.com>
2023-05-02 23:11:52 +0600

    Initial commit

---
+++ src/main/java/com/github/aza06_02/hw_testing/HwTestingApplication.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.hw_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/aza06_02/hw_testing/HwTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.hw_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
